ab472e059ae47a4ad363b75478913771e8a6e3be
xuzon-G <pingpong1435@hotmail.com>
2019-01-31 16:04:19 +0545

    student table added successfully

---
+++ views/Admin/AddStudent.php
@@ -0,0 +1,14 @@
+<?php
+$route=$_SERVER['REQUEST_URI'];
+echo $route;
+include('../layout/header.php');
+?>
+
+
+
+<div class="content-wrapper"> 
+This is Add page of student    
+<div>
+<?php 
+include('../layout/footer.php');
+ ?>
---
+++ views/layout/footer.php
@@ -0,0 +1,39 @@
+</div>
+    !-- jQuery 3 -->
+<script src="/assets/AdminLte/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="/assets/AdminLte/bower_components/jquery-ui/jquery-ui.min.js"></script>
+
+<script src="assets/AdminLte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Morris.js charts -->
+
+<!-- Bootstrap 3.3.7 -->
+
+<script src="/assets/AdminLte/bower_components/moment/min/moment.min.js"></script>
+
+<script src="/assets/AdminLte/dist/js/adminlte.min.js"></script>
+<script src="/assets/AdminLte/bower_components/chart.js/Chart.js"></script>
+
+<!-- Bootstrap 3.3.7 -->
+<script src="/assets/AdminLte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- ChartJS -->
+<script src="/assets/AdminLte/bower_components/chart.js/Chart.js"></script>
+<!-- FastClick -->
+<script src="/assets/AdminLte/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="/assets/AdminLte/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="/assets/AdminLte/dist/js/demo.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+
+<!-- AdminLTE for demo purposes -->
+
+</body>
+</html>
+
